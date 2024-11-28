10f49c04d0f39ce562eac2529bd39b6170cc5e75
juste <sebo.erik@gmail.com>
2024-11-28 11:02:21 +0100

    Initial commit

---
+++ src/main/java/me/minecraft/plugin/harderWardens/HarderWardens.java
@@ -0,0 +1,12 @@
+package me.minecraft.plugin.harderWardens;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HarderWardens extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+    }
+}

c774dc1095d7f5d8f8d5d821c0df904c3d795b2d
justerik <sebo.erik@gmail.com>
2024-11-29 08:15:54 +0100

    Added code for drops and changed max health to 1000

--- src/main/java/me/minecraft/plugin/harderWardens/HarderWardens.java
+++ src/main/java/me/minecraft/plugin/harderWardens/HarderWardens.java
@@ -1,12 +1,71 @@
 package me.minecraft.plugin.harderWardens;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
 
 public final class HarderWardens extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "HarderWardens >> Plugin has been enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void wardenSpawnEvent(EntitySpawnEvent e) {
+
+        if (e.getEntity() instanceof LivingEntity ent) {
+            if (ent.getType() == EntityType.WARDEN) {
+                ent.setCustomName("JOHNY");
+                ent.setCustomNameVisible(true);
+                ent.setMaxHealth(1000);
+                ent.setHealth(1000);
+            }
+        }
+    }
+    @EventHandler
+    public void wardenDeathEvent(EntityDeathEvent e) {
+
+        LivingEntity ent = e.getEntity();
+        Random ran = new Random();
+        int num = ran.nextInt(100);
+
+        ItemStack diamond_chestplate = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        ItemStack diamond_sword = new ItemStack(Material.DIAMOND_SWORD);
+
+        if (ent.getType() == EntityType.WARDEN) {
+            if (num >= 80) {
+                e.getDrops().add(new ItemStack(Material.TOTEM_OF_UNDYING));
+            } else if (num >= 60 && num < 80) {
+                e.getDrops().add(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE, 3));
+            } else if (num >= 40 && num < 60) {
+                e.getDrops().add(new ItemStack(Material.RIB_ARMOR_TRIM_SMITHING_TEMPLATE));
+                e.getDrops().add(new ItemStack(Material.SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE));
+                e.getDrops().add(new ItemStack(Material.DIAMOND_CHESTPLATE));
+                diamond_chestplate.addEnchantment(Enchantment.PROTECTION, 4);
+                diamond_chestplate.addEnchantment(Enchantment.UNBREAKING, 3);
+            } else if (num >= 20 && num < 40) {
+                e.getDrops().add(new ItemStack(Material.OMINOUS_TRIAL_KEY, 2));
+                e.getDrops().add(new ItemStack(Material.DIAMOND_SWORD));
+                diamond_sword.addEnchantment(Enchantment.SHARPNESS, 4);
+                diamond_sword.addEnchantment(Enchantment.UNBREAKING, 3);
+            } else if (num < 20) {
+                e.getDrops().add(new ItemStack(Material.DIAMOND, 6));
+                e.getDrops().add(new ItemStack(Material.NAME_TAG));
+                e.getDrops().add(new ItemStack(Material.COOKED_BEEF, 8));
+            }
+        }
     }
 }

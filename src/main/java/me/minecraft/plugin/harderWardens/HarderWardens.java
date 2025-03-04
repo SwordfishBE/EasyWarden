300601b1ebb3cd3a1d3a9a39541504b442cbcff4
justeriksk <sebo.erik@gmail.com>
2025-03-04 22:28:14 +0100

    Added normal and also custom loot based on warden's difficulty set in the config file

--- src/main/java/me/minecraft/plugin/harderWardens/HarderWardens.java
+++ src/main/java/me/minecraft/plugin/harderWardens/HarderWardens.java
@@ -3,7 +3,6 @@ package me.minecraft.plugin.harderWardens;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -14,10 +13,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collections;
 import java.util.Random;
 
 public final class HarderWardens extends JavaPlugin implements Listener {
@@ -76,13 +78,113 @@ public final class HarderWardens extends JavaPlugin implements Listener {
         String warden_difficulty = this.getConfig().getString("warden_difficulty", "NORMAL");
         if (ent.getType() == EntityType.WARDEN) {
             e.getDrops().clear();
-            if (warden_difficulty.equals("EASY")) { // Loot from wardens on easy difficulty (common drops / uncommon drops)
+            if (warden_difficulty.equals("EASY")) { // Loot from wardens on easy difficulty (Tier 1 drops)
 
-            } else if (warden_difficulty.equals("NORMAL")) { //  Loot from wardens on normal difficulty (uncommon drops / rare drops)
+                e.getDrops().add(new ItemStack(Material.IRON_INGOT, 12)); // 12 Iron Ingots
+                e.getDrops().add(new ItemStack(Material.GOLD_INGOT, 12)); // 12 Gold Ingots
 
-            } else if (warden_difficulty.equals("HARD")) { // Loot from wardens on normal difficulty (rare drops)
+                ItemStack low_level_book1 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta1 = (EnchantmentStorageMeta)low_level_book1.getItemMeta();
+                meta1.addStoredEnchant(Enchantment.SWIFT_SNEAK, 1, true);
+                low_level_book1.setItemMeta(meta1);
+                e.getDrops().add(low_level_book1); // Swift Sneak 1 Book
+
+                ItemStack low_level_book2 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta2 = (EnchantmentStorageMeta)low_level_book2.getItemMeta();
+                meta2.addStoredEnchant(Enchantment.UNBREAKING, 1, true);
+                low_level_book2.setItemMeta(meta2);
+                e.getDrops().add(low_level_book2); // Unbreaking 1 Book
+
+                ItemStack fragment = new ItemStack(Material.CHARCOAL, 1);
+                ItemMeta itemStackMeta = fragment.getItemMeta();
+                itemStackMeta.setDisplayName(ChatColor.DARK_PURPLE + "Warden's Fragment");
+                itemStackMeta.setLore(Collections.singletonList(ChatColor.DARK_GREEN + "A custom drop from Warden. Doesn't have any uses yet."));
+                itemStackMeta.addEnchant(Enchantment.INFINITY, 1, true);
+                itemStackMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                fragment.setItemMeta(itemStackMeta);
+                e.getDrops().add(fragment); // Custom item for easy difficulty wardens - Warden's Fragment
+
+            } else if (warden_difficulty.equals("NORMAL")) { //  Loot from wardens on normal difficulty (Tier 2 drops)
+
+                e.getDrops().add(new ItemStack(Material.NETHERITE_SCRAP, 2)); // 2 Netherite Scraps
+                e.getDrops().add(new ItemStack(Material.DIAMOND, 5)); // 5 Diamonds
+
+                ItemStack mid_level_book1 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta1 = (EnchantmentStorageMeta)mid_level_book1.getItemMeta();
+                meta1.addStoredEnchant(Enchantment.SWIFT_SNEAK, 2, true);
+                mid_level_book1.setItemMeta(meta1);
+                e.getDrops().add(mid_level_book1); // Swift Sneak 2 Book
+
+                ItemStack mid_level_book2 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta2 = (EnchantmentStorageMeta)mid_level_book2.getItemMeta();
+                meta2.addStoredEnchant(Enchantment.PROTECTION, 2, true);
+                mid_level_book2.setItemMeta(meta2);
+                e.getDrops().add(mid_level_book2); // Protection 2 Book
+
+                ItemStack fragment = new ItemStack(Material.ECHO_SHARD, 1);
+                ItemMeta itemStackMeta = fragment.getItemMeta();
+                itemStackMeta.setDisplayName(ChatColor.DARK_PURPLE + "Dark Essence");
+                itemStackMeta.setLore(Collections.singletonList(ChatColor.DARK_GREEN + "A custom drop from Warden. Doesn't have any uses yet."));
+                itemStackMeta.addEnchant(Enchantment.INFINITY, 1, true);
+                itemStackMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                fragment.setItemMeta(itemStackMeta);
+                e.getDrops().add(fragment); // Custom item for normal difficulty wardens - Dark Essence
+
+            } else if (warden_difficulty.equals("HARD")) { // Loot from wardens on hard difficulty (Tier 3 drops)
+
+                ItemStack enc_dia_chestplate = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+                ItemMeta itemStackMeta1 = enc_dia_chestplate.getItemMeta();
+                itemStackMeta1.addEnchant(Enchantment.PROTECTION, 4, true);
+                itemStackMeta1.addEnchant(Enchantment.UNBREAKING, 3, true);
+                enc_dia_chestplate.setItemMeta(itemStackMeta1);
+                e.getDrops().add(enc_dia_chestplate); // Enchanted Diamond Chestplate
+
+                ItemStack high_level_book1 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta1 = (EnchantmentStorageMeta)high_level_book1.getItemMeta();
+                meta1.addStoredEnchant(Enchantment.SWIFT_SNEAK, 3, true);
+                high_level_book1.setItemMeta(meta1);
+                e.getDrops().add(high_level_book1); // Swift Sneak 3 Book
+
+                ItemStack high_level_book2 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta2 = (EnchantmentStorageMeta)high_level_book2.getItemMeta();
+                meta2.addStoredEnchant(Enchantment.SHARPNESS, 4, true);
+                high_level_book2.setItemMeta(meta2);
+                e.getDrops().add(high_level_book2); // Sharpness 4 Book
+
+                ItemStack fragment2 = new ItemStack(Material.AMETHYST_SHARD, 1);
+                ItemMeta itemStackMeta2 = fragment2.getItemMeta();
+                itemStackMeta2.setDisplayName(ChatColor.DARK_PURPLE + "Void Fragment");
+                itemStackMeta2.setLore(Collections.singletonList(ChatColor.DARK_GREEN + "A custom drop from Warden. Doesn't have any uses yet."));
+                itemStackMeta2.addEnchant(Enchantment.INFINITY, 1, true);
+                itemStackMeta2.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                fragment2.setItemMeta(itemStackMeta2);
+                e.getDrops().add(fragment2); // Custom item for hard difficulty wardens - Void Fragment
 
             } else { // if difficulty not set correctly, default difficulty (normal) will be used
+
+                e.getDrops().add(new ItemStack(Material.NETHERITE_SCRAP, 2)); // 2 Netherite Scraps
+                e.getDrops().add(new ItemStack(Material.DIAMOND, 5)); // 5 Diamonds
+
+                ItemStack mid_level_book1 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta1 = (EnchantmentStorageMeta)mid_level_book1.getItemMeta();
+                meta1.addStoredEnchant(Enchantment.SWIFT_SNEAK, 2, true);
+                mid_level_book1.setItemMeta(meta1);
+                e.getDrops().add(mid_level_book1); // Swift Sneak 2 Book
+
+                ItemStack mid_level_book2 = new ItemStack(Material.ENCHANTED_BOOK, 1);
+                EnchantmentStorageMeta meta2 = (EnchantmentStorageMeta)mid_level_book2.getItemMeta();
+                meta2.addStoredEnchant(Enchantment.PROTECTION, 2, true);
+                mid_level_book2.setItemMeta(meta2);
+                e.getDrops().add(mid_level_book2); // Protection 2 Book
+
+                ItemStack fragment = new ItemStack(Material.ECHO_SHARD, 1);
+                ItemMeta itemStackMeta = fragment.getItemMeta();
+                itemStackMeta.setDisplayName(ChatColor.DARK_PURPLE + "Dark Essence");
+                itemStackMeta.setLore(Collections.singletonList(ChatColor.DARK_GREEN + "A custom drop from Warden. Doesn't have any uses yet."));
+                itemStackMeta.addEnchant(Enchantment.INFINITY, 1, true);
+                itemStackMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                fragment.setItemMeta(itemStackMeta);
+                e.getDrops().add(fragment); // Custom item for normal difficulty wardens - Dark Essence
 
             }
         }
